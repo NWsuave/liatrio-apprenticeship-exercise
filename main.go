8732e33a0279c74a8234905f175be50d705aa427
Nico Whitmarsh <nicow5544@gmail.com>
2026-02-20 17:01:45 -0800

    feat: implement Fiber HTTP endpoint returning name and timestamp

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import(
+	"time"
+	"github.com/gofiber/fiber/v3"
+)
+
+func rootHandler(c fiber.Ctx) error{
+	return c.JSON(fiber.Map{
+		"message": "My name is Nico Whitmarsh",
+		"timestamp": time.Now().Unix(),
+	})
+}
+
+func main() {
+	app := fiber.New()
+	app.Get("/", rootHandler)
+	app.Listen(":3000")
+}
